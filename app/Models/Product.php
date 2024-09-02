af944b00c4c8e395132a53ba413f0daadfeabf3f
Kamel Faour <10121641@mu.edu.lb>
2024-09-02 16:47:38 +0300

    Product Resource done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -29,7 +29,7 @@ class Product extends Model
         'images' => 'array',
     ];
 
-    public function categpry(){
+    public function category(){
         return $this->belongsTo(Category::class);
     }
 
